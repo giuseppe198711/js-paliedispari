57a4055779c46d12e371ea97a64e4995e01a7fa4
Giuseppe <giuseppedf8711@gmail.com>
2020-08-27 17:25:08 +0200

    fine parola palindroma

--- pali/script.js
+++ pali/script.js
@@ -4,10 +4,25 @@
 var parola = prompt("Inserisci la parola");
 console.log(parola);
 
-// devo fare la differenza tra una parola normale e una palindroma
-function checkPalindrom(palindrom) {
+function ilPalindromo(parola) {
+  // leggo la parola al contrario
+  // creo un ciclo for che parte dalla fine della parola;
+  // continua fino a che non arrivo alla prima lettera(0);
+  // decremento di uno ogni volta;
+  var parolaReverse = ""; //sto creando una variabile esterna al ciclo perchè altimenti  se la metto al'interno del for ad ogni ciclo del for si andrebbe a ricreare perdendo il contenuto
 
-for (var i = palindrom.length; i > 0; i--)
+  for (var i = parola.length-1; i >= 0; i--) {
+    // i parte con un valore numerico pari al numero di lettere all interno della parola
+    // mi salvo la mia parola al contrario
+    parolaReverse = parolaReverse + parola[i]; //possiamo scrivere anche parolaReverse += parola[i];
+    // i decrementa di 1
+  }
+return parolaReverse;
+}
 
-if( palindrom[i])
+// confronto la parola con quella di origine
+if (ilPalindromo(parola) == parola) {
+  console.log("la parola è palindroma");
+} else {
+  console.log("la parola non è palindroma");
 }
