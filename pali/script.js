f247fd609d5de2fe0716d0d14fe67c03eedcd477
Giuseppe <giuseppedf8711@gmail.com>
2020-08-27 15:52:28 +0200

    inizio

---
+++ pali/script.js
@@ -0,0 +1,7 @@
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// faccio inserire all'utente la parola
+var parola = prompt("Inserisci la parola");
+console.log(parola);
+
+// devo fare la differenza tra una parola normale e una palindroma
